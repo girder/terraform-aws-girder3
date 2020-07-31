5613d387d7bc7709e8c1c31c39b8fb2c8abce1eb
Brian Helba <brian.helba@kitware.com>
2020-07-30 21:57:03 -0400

    Add defaults to some sub-module variables

--- variables.tf
+++ variables.tf
@@ -3,21 +3,11 @@ variable "project_slug" {
   description = "A slugified name, used to label AWS resources."
 }
 
-variable "ec2_instance_type" {
+variable "route53_zone_id" {
   type        = string
-  default     = "t3.medium"
-  description = "The EC2 instance type for the server."
+  description = "The Route 53 zone ID to create new DNS records within."
 }
-variable "ec2_ami" {
-  type        = string
-  default     = "ami-0ac80df6eff0e70b5"
-  description = "The base AMI ID for the server."
-}
-variable "ec2_volume_size" {
-  type        = number
-  default     = null
-  description = "The size, in GB, of the root EBS volume for the server."
-}
+
 variable "ssh_public_key" {
   type        = string
   description = "An SSH public key, to be installed on the server."
@@ -27,9 +17,23 @@ variable "subdomain_name" {
   type        = string
   description = "The DNS subdomain name where the server will be available."
 }
-variable "route53_zone_id" {
+
+variable "ec2_instance_type" {
   type        = string
-  description = "The Route 53 zone ID to create new DNS records within."
+  default     = "t3.medium"
+  description = "The EC2 instance type for the server."
+}
+
+variable "ec2_ami" {
+  type        = string
+  default     = "ami-07df16d0682f1fa59"
+  description = "The base AMI ID for the server."
+}
+
+variable "ec2_volume_size" {
+  type        = number
+  default     = 40
+  description = "The size, in GB, of the root EBS volume for the server."
 }
 
 variable "extra_upload_origins" {
