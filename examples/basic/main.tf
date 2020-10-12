21176de2c6ccf2fe6e68a23c2b4645dd1f1d7af7
Brian Helba <brian.helba@kitware.com>
2020-10-12 13:04:55 -0400

    Make the example slightly easier to use

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   region = "us-east-1"
+  # Must set AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY envvars
 }
 
 data "aws_route53_zone" "domain" {
   # This must be created by hand in the AWS console
-  zone_id = "Z123456789"
+  name = "rootdomain.test"
 }
 
 data "local_file" "ssh_pub" {
