c1f34d8a11635e68a48d68363d23d8aa95f55c07
Brian Helba <brian.helba@kitware.com>
2021-02-09 10:56:47 -0500

    Add a lifecycle rule to the assetstore, to clean up incomplete uploads

--- modules/assetstore/main.tf
+++ modules/assetstore/main.tf
@@ -12,6 +12,11 @@ resource "aws_s3_bucket" "assetstore" {
     }
   }
 
+  lifecycle_rule {
+    enabled                                = true
+    abort_incomplete_multipart_upload_days = 7
+  }
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["POST", "PUT"]
