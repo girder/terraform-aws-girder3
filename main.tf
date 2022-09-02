02e4195d2255d55d09cc0221ff039623b1ef3857
Brian Helba <brian.helba@kitware.com>
2022-09-02 09:48:06 -0400

    Move the SMTP module to "terraform-heroku-girder4"
    See: https://github.com/girder/terraform-heroku-girder4/pull/59

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ data "aws_iam_policy_document" "server_assetstore" {
 }
 
 module "smtp" {
-  source = "./modules/smtp"
+  source = "girder/girder4/heroku//modules/smtp"
 
   project_slug    = var.project_slug
   route53_zone_id = var.route53_zone_id
