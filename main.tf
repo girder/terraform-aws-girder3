e378ec69d3ba8c04d89f669ddc2dcf53e27dc83a
Brian Helba <brian.helba@kitware.com>
2023-05-24 09:06:52 -0400

    Pin the SMTP external module version

--- main.tf
+++ main.tf
@@ -48,7 +48,8 @@ data "aws_iam_policy_document" "server_assetstore" {
 }
 
 module "smtp" {
-  source = "girder/girder4/heroku//modules/smtp"
+  source  = "girder/girder4/heroku//modules/smtp"
+  version = "0.13.0"
 
   project_slug    = var.project_slug
   route53_zone_id = var.route53_zone_id
